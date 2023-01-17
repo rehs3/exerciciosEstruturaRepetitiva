5e06d40e38b495f451346cbb8959232ee70d052a
regin <regin@DESKTOP-5HFBV50>
2023-01-17 15:16:58 -0300

    Adicionar arquivos de projeto.

---
+++ exerciciosEstruturaRepetitiva/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace repetitivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a senha: ");
+            int senha= int.Parse(Console.ReadLine());
+
+            while( senha != 2002)
+            {
+                Console.WriteLine("Senha incorreta!!");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Senha correta!!");
+        }
+    }
+}
